dd46bb5dabcb71615d4fd026012fa970ac786c17
Jazmine Zurita <jazzurita1@gmail.com>
2016-02-24 14:50:16 -0500

    File /Users/Jazz/Documents/workspace2/Assignment2/src/Link.java changed at Wed Feb 24 14:50:16 EST 2016

--- src/Link.java
+++ src/Link.java
@@ -3,9 +3,69 @@
  */
 
 /**
- * @author Jazz
+ * @author Jazmine Zurita and Jessica McCready
  *
  */
-public class Link {
+public class Link<Point> {
+	private Point e; //val for this node
+	private Link<Point> forward;  //should point to next node in list
+	
+	/**
+	 * Constructor that takes in a point and a position
+	 * @param it
+	 * @param n
+	 */
+	public Link(Point it, Link<Point> n)
+	{
+		e = it;
+		forward = n;
+	}
+	
+	/**
+	 * Constructor that only takes inthe position
+	 * @param n
+	 */
+	public Link(Link<Point> n)
+	{
+		e = null;
+		forward = n;
+	}
+	
+	/**
+	 * Returns the point
+	 * @return e
+	 */
+	public Point element()
+	{
+		return e;
+	}
+	
+	/**
+	 * Sets the point to a specific instance
+	 * @param it the point it will be set to
+	 * @return the new value 
+	 */
+	public Point setElement(Point it)
+	{
+		return e = it;
+	}
+	
+	/**
+	 * Returns the next link
+	 * @return next link
+	 */
+	public Link<Point> next()
+	{
+		return forward;
+	}
 
+	/**
+	 * Sets the next link
+	 * @param n the next link
+	 * @return new next
+	 */
+	public Link<Point> setNext(Link<Point> n)
+	{
+		return forward = n;
+	}
 }
