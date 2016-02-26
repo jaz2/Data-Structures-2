d2ee1b6d0302d5b2b5babce1773959a714b9a97c
Jazmine Zurita <jazzurita1@gmail.com>
2016-02-26 14:59:13 -0500

    File /Users/Jazz/Documents/workspace2/Assignment2/src/Flyweight.java changed at Fri Feb 26 14:59:11 EST 2016

---
+++ src/Flyweight.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jazz
+ *
+ */
+public class Flyweight implements PRQuadNode{
+
+	//global static rep of itself to return that
+	global Flyweight fly = new Flyweight();
+	
+	@Override
+	public boolean isLeaf() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public PRQuadNode insert(Point p, int x, int y, int length) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
