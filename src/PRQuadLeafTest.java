2249e8cd7fbd111c092ccc16267fd5a5877da7b4
Jazmine Zurita <jazzurita1@gmail.com>
2016-02-27 01:23:16 -0500

    File /Users/Jazz/Documents/workspace2/Assignment2/src/PRQuadLeafTest.java changed at Sat Feb 27 01:23:12 EST 2016

--- src/PRQuadLeafTest.java
+++ src/PRQuadLeafTest.java
@@ -20,9 +20,18 @@ public class PRQuadLeafTest {
 	public void setUp() throws Exception {
 	}
 
+	/**
+	 * Leaf should already have a point,
+	 * calls leaf to have another point added
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInsertFirs() {
+		Point p = new Point(null, 3, 4);
+		PRQuadLeaf le = new PRQuadLeaf(p);
+		
+		Point k = new Point(null, 2, 3);
+		le.insert(p, 2, 3, 9);
+		assertEquals(PRQuadLeaf.class, this.getClass());
 	}
 
 }
