be03a935c073195eaed7583c720aeb5162ea83f8
Jazmine Zurita <jazzurita1@gmail.com>
2016-02-28 15:10:03 -0500

    File /Users/Jazz/Documents/workspace2/Assignment2/src/CommandProcessor.java changed at Sun Feb 28 15:10:03 EST 2016

--- src/CommandProcessor.java
+++ src/CommandProcessor.java
@@ -136,7 +136,7 @@ public class CommandProcessor {
                 
             } 
             else if (str.contains("dump")) {
-                db.skip.dump();
+                db.dump();
 //            	System.out.println("SkipList dump:\n" + "Node has depth 1, Value (null)\n"
 //                + "SkipList size is: 0\n" + "QuadTree dump:\n"
 //                + "Node at 0, 0, 1024: Empty\n" + "1 quadtree nodes printed");
