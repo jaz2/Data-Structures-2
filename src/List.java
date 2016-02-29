7c04ea21ed8be0e3a07ddbe2977c7a5cfab513a0
Jazmine Zurita <jazzurita1@gmail.com>
2016-02-29 18:44:38 -0500

    File /Users/Jazz/Documents/workspace2/Assignment2/src/List.java changed at Mon Feb 29 18:44:38 EST 2016

--- src/List.java
+++ src/List.java
@@ -1,3 +1,5 @@
+
+
 /**
  * 
  */
@@ -7,35 +9,72 @@
  * @version Feb 22 2016
  *
  */
-public class List<Point> {
-	
-	private Point head;
-	private Point curr;
-	private int size;
-	
-	public List<Point>()
-	{
-		size = 0;
-	}
-	
-	/**
-	 * Inserts into the list
-	 * @param p
-	 */
-	public void insert(Point p)
-	{}
-	
-	public Point moveToPos(int i)
-	{}
-	
-	/**
-	 * Returns the size
-	 * @return
-	 */
-	public int size()
-	{
-		return size;
-	}
-	
+public class List {
 
+    //private Point head;
+    private Point curr;
+    private int size;
+    private Point[] l;
+
+    public List()
+    {
+        l = new Point[0];
+        size = 0;
+    }
+
+    /**
+     * Inserts into the list
+     * @param p
+     */
+    public void insert(Point p)
+    {
+        l[size] = p;
+        size++;
+    }
+
+    /**
+     * 
+     * @param i the position 
+     * @return the point at the pos
+     */
+    public Point moveToPos(int i)
+    {
+        return l[i];
+    }
+
+    public boolean remove(Point p)
+    {
+        //Point n = null;
+        Boolean found = false;
+        for (int i = 0; i < size; i++)
+        {
+            if (l[i] == p)
+            {
+                //n = p;
+                if (i == size - 1)
+                {
+                    size--;
+                }
+                else 
+                {
+                    for (int j = i; j < size; j++)
+                    {
+                        l[j] = l[j + 1];
+                    }
+                    size--;
+                }
+                found = true;
+            }
+        }
+        return found;
+    }
+
+    /**
+     * Returns the size
+     * @return
+     */
+    public int length()
+    {
+        return size;
+    }
 }
