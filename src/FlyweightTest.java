74ca1a2e241afbd3bc469fa9ab83d6853ec3bbd7
Jazmine Zurita <jazzurita1@gmail.com>
2016-02-29 17:02:08 -0500

    File /Users/Jazz/Documents/workspace2/Assignment2/src/FlyweightTest.java changed at Mon Feb 29 17:02:07 EST 2016

--- src/FlyweightTest.java
+++ src/FlyweightTest.java
@@ -9,27 +9,27 @@ import student.TestCase;
  */
 
 /**
- * @author Jazz
+ * @author Jazmine Zurita and Jessica McCready
+ * @version Feb 22 2016
  *
  */
-public class FlyweightTest extends TestCase{
+public class FlyweightTest extends TestCase {
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	/**
-	 * Tests when something is inserted
-	 */
-	@Test
-	public void testInsert() {
-		Flyweight fl = new Flyweight();
-		Point p = new Point(null, 3, 3);
-		fl.insert(p, 3, 3, 4);
-		assertEquals(PRQuadLeaf.class, fl.getInstance().getClass());
-	}
+    /**
+     * Tests when something is inserted
+     */
+    @Test
+    public void testInsert() {
+        Flyweight fl = new Flyweight();
+        Point p = new Point(null, 3, 3);
+        fl.insert(p, 3, 3, 4);
+        assertEquals(PRQuadLeaf.class, fl.getInstance().getClass());
+    }
 
 }
