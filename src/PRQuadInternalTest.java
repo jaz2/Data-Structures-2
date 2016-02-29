0fee1cd3b70cc998fc2ad35835fe67d1f539c157
Jazmine Zurita <jazzurita1@gmail.com>
2016-02-29 17:54:26 -0500

    File /Users/Jazz/Documents/workspace2/Assignment2/src/PRQuadInternalTest.java changed at Mon Feb 29 17:54:26 EST 2016

--- src/PRQuadInternalTest.java
+++ src/PRQuadInternalTest.java
@@ -51,7 +51,7 @@ public class PRQuadInternalTest extends TestCase {
      */
     @Test
     public void testSw() {
-        Point p = new Point(null, 4, 900);
+        Point p = new Point(null, 4, 1000);
         PRQuadInternal i = new PRQuadInternal();
         i.insert(p, 0, 0, 1024);
         assertEquals(PRQuadLeaf.class, i.sw().getClass());
