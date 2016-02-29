5e9e6b3672b0f83f2f8a3e9570d73f4ed80843d3
Jazmine Zurita <jazzurita1@gmail.com>
2016-02-29 18:23:14 -0500

    File /Users/Jazz/Documents/workspace2/Assignment2/src/List.java changed at Mon Feb 29 18:23:14 EST 2016

---
+++ src/List.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jazmine Zurita and Jessica McCready
+ * @version Feb 22 2016
+ *
+ */
+public class List<Point> {
+	
+	private Point head;
+	private Point curr;
+	private int size;
+	
+	public List<Point>()
+	{
+		size = 0;
+	}
+	
+	/**
+	 * Inserts into the list
+	 * @param p
+	 */
+	public void insert(Point p)
+	{}
+	
+	public Point moveToPos(int i)
+	{}
+	
+	/**
+	 * Returns the size
+	 * @return
+	 */
+	public int size()
+	{
+		return size;
+	}
+	
+
+}
