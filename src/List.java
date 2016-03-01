3057160f155d7775ba416b63d29784d1094f5090
Jazmine Zurita <jazzurita1@gmail.com>
2016-02-29 19:05:28 -0500

    File /Users/Jazz/Documents/workspace2/Assignment2/src/List.java changed at Mon Feb 29 19:05:28 EST 2016

--- src/List.java
+++ src/List.java
@@ -11,70 +11,88 @@
  */
 public class List {
 
-    //private Point head;
-    private Point curr;
-    private int size;
-    private Point[] l;
+	//private Point head;
+	private Point curr;
+	private int size;
+	private Point[] l;
 
-    public List()
-    {
-        l = new Point[3];
-        size = 0;
-    }
+	public List()
+	{
+		l = new Point[3];
+		size = 0;
+	}
 
-    /**
-     * Inserts into the list
-     * @param p
-     */
-    public void insert(Point p)
-    {
-        l[size] = p;
-        size++;
-    }
+	/**
+	 * Inserts into the list
+	 * @param p
+	 */
+	public void insert(Point p)
+	{
+		if (l[size] == null)
+		{
+			size = size * 2;
+			Point[] n = new Point[size];
+			for (int i = 0; i < size; i++)
+			{
+				n[i] = l[i];
+			}
+			n[(size / 2) + 1] = p;
+			for (int j = (size / 2) + 1; j < size; j++)
+			{
+				l[j] = n[j];
+			}
+			l = n;
+		}
+		else 
+		{
+			l[size] = p;
+			size++;
+		}
+	}
 
-    /**
-     * 
-     * @param i the position 
-     * @return the point at the pos
-     */
-    public Point moveToPos(int i)
-    {
-        return l[i];
-    }
+	/**
+	 * 
+	 * @param i the position 
+	 * @return the point at the pos
+	 */
+	public Point moveToPos(int i)
+	{
+		return l[i];
+	}
 
-    public boolean remove(Point p)
-    {
-        //Point n = null;
-        Boolean found = false;
-        for (int i = 0; i < size; i++)
-        {
-            if (l[i] == p)
-            {
-                //n = p;
-                if (i == size - 1)
-                {
-                    size--;
-                }
-                else 
-                {
-                    for (int j = i; j < size; j++)
-                    {
-                        l[j] = l[j + 1];
-                    }
-                    size--;
-                }
-                found = true;
-            }
-        }
-        return found;
-    }
+	public boolean remove(Point p)
+	{
+		//Point n = null;
+		Boolean found = false;
+		for (int i = 0; i < size; i++)
+		{
+			if (l[i] == p)
+			{
+				//n = p;
+				if (i == size - 1)
+				{
+					size--;
+				}
+				else 
+				{
+					for (int j = i; j < size; j++)
+					{
+						l[j] = l[j + 1];
+					}
+					size--;
+				}
+				found = true;
+			}
+		}
+		return found;
+	}
 
-    /**
-     * Returns the size
-     * @return
-     */
-    public int length()
-    {
-        return size;
-    }
+	/**
+	 * Returns the size
+	 * @return
+	 */
+	public int length()
+	{
+		return size;
+	}
 }
