842f890e93de928638297b666498ba571eba422f
Jazmine Zurita <jazzurita1@gmail.com>
2016-03-02 18:20:08 -0500

    File /Users/Jazz/Documents/workspace2/Assignment2/src/Flyweight.java changed at Wed Mar 02 18:20:08 EST 2016

--- src/Flyweight.java
+++ src/Flyweight.java
@@ -112,12 +112,4 @@ public class Flyweight implements PRQuadNode {
     {
         return 1;
     }
-
-    /**
-     * Returns 0 because it's empty
-     */
-    public int count()
-    {
-    	return 0;
-    }
 }
