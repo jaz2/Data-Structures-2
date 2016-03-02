b5078ab72bcd5a1eca4de062aa31de5aa7ef3050
Jazmine Zurita <jazzurita1@gmail.com>
2016-03-01 23:35:57 -0500

    File /Users/Jazz/Documents/workspace2/Assignment2/src/PRQuadLeafTest.java changed at Tue Mar 01 23:35:57 EST 2016

--- src/PRQuadLeafTest.java
+++ src/PRQuadLeafTest.java
@@ -234,7 +234,7 @@ public class PRQuadLeafTest extends TestCase{
 		Point p = new Point("a", 1000, 2);
 		PRQuadLeaf lf = new PRQuadLeaf(p);		
 
-		assertEquals(0, lf.regionSearch(100, 100, 10, 10, 0, 0, 1024));
+		assertEquals(1, lf.regionSearch(100, 100, 10, 10, 0, 0, 1024));
 	}
 
 	public void regionSearch()
