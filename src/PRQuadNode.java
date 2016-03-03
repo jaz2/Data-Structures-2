6f75eec630026f47dcc0374ffb4524e982e2d703
Jazmine Zurita <jazzurita1@gmail.com>
2016-03-02 20:21:13 -0500

    File /Users/Jazz/Documents/workspace2/Assignment2/src/PRQuadNode.java changed at Wed Mar 02 20:21:13 EST 2016

--- src/PRQuadNode.java
+++ src/PRQuadNode.java
@@ -18,7 +18,7 @@ public interface PRQuadNode {
 	
 	public PRQuadNode insert(Point p, int x, int y, int length);
 	
-	public PRQuadNode remove(int x, int y, int tx, int ty, int len);
+	public PRQuadNode remove(String n, int x, int y, int tx, int ty, int len);
 	
 	public PRQuadNode merge();
 }
