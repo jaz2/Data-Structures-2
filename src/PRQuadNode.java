3a49675a3a8a702398b8542f46c8abfe9a8d5ccd
Jazmine Zurita <jazzurita1@gmail.com>
2016-03-04 15:25:13 -0500

    File /Users/Jazz/Documents/workspace2/Assignment2/src/PRQuadNode.java changed at Fri Mar 04 15:25:13 EST 2016

--- src/PRQuadNode.java
+++ src/PRQuadNode.java
@@ -3,15 +3,35 @@
  */
 
 /**
- * @author Jazz
+ * @author Jazmine Zurita and Jessica McCready
+ * @version March 2 2016
  *
  */
 public interface PRQuadNode {
 	
+	/**
+	 * checks if the node is a leaf
+	 * @return true or false
+	 */
 	public boolean isLeaf();
 	
+	/**
+	 * Checks if the node is empty
+	 * @return true or false
+	 */
 	public boolean isEmpty();
 	
+	/**
+	 * Searches within a region for points
+	 * @param x the x coord of region
+	 * @param y the y coord of region
+	 * @param w the width of region
+	 * @param l the length of region
+	 * @param nx the tree x
+	 * @param ny the tree y
+	 * @param nl the tree length
+	 * @return the number of things in region
+	 */
 	public int regionSearch(int x, int y, int w, int l, int nx, int ny, int nl);
 	
 	public void duplicates();
