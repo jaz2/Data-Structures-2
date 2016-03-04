6187411efdedb7b11eac54dae6ed41cb47a4dcd9
Jazmine Zurita <jazzurita1@gmail.com>
2016-03-04 14:52:14 -0500

    File /Users/Jazz/Documents/workspace2/Assignment2/src/Database.java changed at Fri Mar 04 14:52:14 EST 2016

--- src/Database.java
+++ src/Database.java
@@ -9,112 +9,112 @@
 
 public class Database
 {
-	/**
-	 * The SkipList
-	 */
-	public SkipList<String, Point> skip; 
+    /**
+     * The SkipList
+     */
+    public SkipList<String, Point> skip; 
 
-	/**
-	 * The PRQuadtree instance
-	 */
-	public PRQuadtree pr;
+    /**
+     * The PRQuadtree instance
+     */
+    public PRQuadtree pr;
 
-	/**
-	 * The Database constructor which creates a SkipList
-	 */
-	public Database()
-	{
-		skip = new SkipList<String, Point>();
-		pr = new PRQuadtree(0, 0, 1024);
-	}
+    /**
+     * The Database constructor which creates a SkipList
+     */
+    public Database()
+    {
+        skip = new SkipList<String, Point>();
+        pr = new PRQuadtree(0, 0, 1024);
+    }
 
-	/**
-	 * Inserts using the SkipList
-	 * 
-	 * @param id the key
-	 * @param x the x coord
-	 * @param y the y coord
-	 * @return
-	 */
-	public void insert(String id, int x, int y)
-	{
-		Point p = new Point(id, x, y);
-		KVPair<String, Point> k = new KVPair<String, Point>(id, p);
-		skip.insert(k); 
-		pr.insert(p);
-	}
+    /**
+     * Inserts using the SkipList
+     * 
+     * @param id the key
+     * @param x the x coord
+     * @param y the y coord
+     * @return
+     */
+    public void insert(String id, int x, int y)
+    {
+        Point p = new Point(id, x, y);
+        KVPair<String, Point> k = new KVPair<String, Point>(id, p);
+        skip.insert(k); 
+        pr.insert(p);
+    }
 
-	/**
-	 * Dumps using the skip list 
-	 * and quad tree
-	 */
-	public void dump()
-	{
-		skip.dump();
-		pr.dump();
-	}
+    /**
+     * Dumps using the skip list 
+     * and quad tree
+     */
+    public void dump()
+    {
+        skip.dump();
+        pr.dump();
+    }
 
-	/**
-	 * Call the pr quad tree region search
-	 * @param xc the xcoord
-	 * @param yc the ycoord
-	 * @param w the width
-	 * @param l the lenfth
-	 */
-	public void regionSearch(int xc, int yc, int w, int l)
-	{
-		pr.regionSearch(xc, yc, w, l);
-	}
+    /**
+     * Call the pr quad tree region search
+     * @param xc the xcoord
+     * @param yc the ycoord
+     * @param w the width
+     * @param l the lenfth
+     */
+    public void regionSearch(int xc, int yc, int w, int l)
+    {
+        pr.regionSearch(xc, yc, w, l);
+    }
 
-	/**
-	 * Calls the pr quad trees duplicates
-	 */
-	public void duplicates()
-	{
-		pr.duplicates();
-	}
+    /**
+     * Calls the pr quad trees duplicates
+     */
+    public void duplicates()
+    {
+        pr.duplicates();
+    }
 
-	/**
-	 * Searches for the key
-	 * @param k the key
-	 */
-	public void search(String k)
-	{
-		skip.search(k);
-	}
+    /**
+     * Searches for the key
+     * @param k the key
+     */
+    public void search(String k)
+    {
+        skip.search(k);
+    }
 
-	/**
-	 * Searches through the SkipList first 
-	 * and then retrieves the point to find 
-	 * the coordinates
-	 * @param name
-	 */
-	public void removeN(String name)
-	{ //use this to get the point and remove it in quadtree
-		skip.removeByName(name);
-		if (skip.found == true)
-		{
-			pr.remove(name, skip.val.x, skip.val.y);
-		}          
-	}
+    /**
+     * Searches through the SkipList first 
+     * and then retrieves the point to find 
+     * the coordinates
+     * @param name
+     */
+    public void removeN(String name)
+    { //use this to get the point and remove it in quadtree
+        skip.removeByName(name);
+        if (skip.found == true)
+        {
+            pr.remove(name, skip.val.x, skip.val.y);
+        }          
+    }
 
-	/**
-	 * Removes in the quad tree returns 
-	 * node to get name to remove 
-	 * by name in skip
-	 * @param x
-	 * @param y
-	 */
-	public void removeC(int x, int y)
-	{
-		pr.remove(null, x, y);
-		if (PRQuadLeaf.nam != null)
-		{
-			skip.removeByName(PRQuadLeaf.nam);
-		}
-		else
-		{
-			System.out.println("Point not found: (" + x + ", " + y + ")");
-		}
-	}
+    /**
+     * Removes in the quad tree returns 
+     * node to get name to remove 
+     * by name in skip
+     * @param x
+     * @param y
+     */
+    public void removeC(int x, int y)
+    {
+        pr.remove(null, x, y);
+        if (PRQuadLeaf.nam != null)
+        {
+            skip.removeByName(PRQuadLeaf.nam);
+        }
+        else
+        {
+            System.out.println("Point not found: (" + x + ", " + y + ")");
+        }
+    }
 }
