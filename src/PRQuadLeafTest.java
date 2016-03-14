b93936f5c6a2fa82f3b4b378b98eac0d1a414a70
Jazmine Zurita <jazzurita1@gmail.com>
2016-03-13 21:08:27 -0400

    File /Users/Jazz/Documents/workspace2/Assignment2/src/PRQuadLeafTest.java changed at Sun Mar 13 21:08:27 EDT 2016

--- src/PRQuadLeafTest.java
+++ src/PRQuadLeafTest.java
@@ -634,7 +634,9 @@ public class PRQuadLeafTest extends TestCase {
         PRQuadtree tree = new PRQuadtree(0, 0, 1024);
         tree.insert(p);
         tree.insert(new Point(null, 800, 601));
-        tree.remove(null, 800, 601);
+        Point n = new Point(null, 800, 601);
+        PRQuadLeaf lf = new PRQuadLeaf(n);
+        lf.remove(null, 800, 601, 0, 0, 1024);
         tree.regionSearch(100, 100, 800, 600); 
         String output = systemOut().getHistory();
         assertEquals("Points intersecting region (100, 100, 800, 600):\n"
